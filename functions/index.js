c3d9615ed140b8a5da481b90a7b99b4bb7fb8a7d
kevchang <kevchang@cs.stonybrook.edu>
2018-06-14 18:57:24 -0400

    template finished

--- functions/index.js
+++ functions/index.js
@@ -64,7 +64,8 @@ exports.generate = functions.https.onRequest((request, response) => {
 		arr[arr.length] = "atrial fibrillation"
 	}
 	
-	p1 += arrFormatter(arr) + ".";
+	p1 += arrFormatter(arr);
+	p1 = endSentence(p1);
 	
 	
 	//Chest Pain details
@@ -135,25 +136,74 @@ exports.generate = functions.https.onRequest((request, response) => {
 			}
 			//PLURAL no numbers: seconds
 			else if(b.d0){
-				p2 += " lasting " + duration + "s ";
+				p2 += " lasting " + duration + "s";
 			}
 			//PLURAL one number: 2 seconds
 			else if(arr.length == 1){
-				p2 += " lasting " + arr[0] +" "+ duration + "s ";
+				p2 += " lasting " + arr[0] +" "+ duration + "s";
 			}
 			//PLURAL range: 3-5 seconds
 			else if(arr.length >= 2){
-				p2 += " lasting " + arr[0] + "-" + arr[arr.length-1]+" "+ duration + "s ";
+				p2 += " lasting " + arr[0] + "-" + arr[arr.length-1]+" "+ duration + "s";
 			}
 			
 			p1 += p2;
 		}		
 	}else{
-		p1 += " Patient denies chest pain."
+		p1 += " Patient denies chest pain"
 	}
+	p1 = endSentence(p1);
+	//SOB
+	var sentence1 = b.s0 + b.s1;
+	if(sentence1 != "") sentence1 = endSentence(sentence1);
+	//Palpitations
+	arr = new Array();
+	if(b.s2!="") arr[arr.length] = b.s2;
+	if(b.s3!="") arr[arr.length] = b.s3;
+	if(b.s4!="") arr[arr.length] = b.s4;
+	if(b.s5!="") arr[arr.length] = b.s5;
+	if(b.s6!="") arr[arr.length] = b.s6;
+	var sentence2 = arrFormatNoAnd(arr);
+	if(sentence2 != "") sentence2 = endSentence(sentence2);
+	sentence1+=sentence2;
 	
+	//Syncope
+	if(b.s7 != "") sentence1+=b.s7;
+	p1+=endSentence(sentence1);
 
 
+	//Last Page
+	arr = new Array();
+	if(b.e0) arr[arr.length] = b.e0;
+	if(b.e1) arr[arr.length] = b.e1
+	if(b.e2) arr[arr.length] = b.e2
+	if(b.e3) arr[arr.length] = b.e3
+	if(b.e4) arr[arr.length] = b.e4
+	if(b.e5) arr[arr.length] = b.e5
+	if(b.e6) arr[arr.length] = b.e6
+	if(b.e7) arr[arr.length] = b.e7
+	if(b.e8) arr[arr.length] = b.e8
+	if(b.e9) arr[arr.length] = b.e9
+	if(b.e10) arr[arr.length] = b.e10
+	if(b.e17) arr[arr.length] = b.e17
+	var combined = "<br>";
+	for(var i = 0; i < arr.length;i++){
+		let temp = "(" + (i+1) + ") " + arr[i] + "<br>"
+		combined = combined + temp
+	}
+	p1+=combined;
+	
+	//arrFormatN
+	arr = new Array();
+	if(b.e11) arr[arr.length] = b.e11;
+	if(b.e12) arr[arr.length] = b.e12;
+	if(b.e13) arr[arr.length] = b.e13;
+	if(b.e14) arr[arr.length] = b.e14;
+	if(b.e15) arr[arr.length] = b.e15;
+	if(b.e16) arr[arr.length] = b.e16;
+	if(arr.length != 0) 
+		var str = "<br>I advised patient to have " + arrFormatNoAnd(arr);
+		p1 += endSentence(str);
 	
 	response.send(p1);
 });
@@ -172,5 +222,30 @@ function arrFormatter(arr){
 	string+=" and "
 	string+= arr[arr.length-1];
 	
+	return string;
+}
+
+function endSentence(string){
+	if(string=="") return "";
+	if(string.charAt(string.length-1) == '.'){
+		return string;
+	}else{
+		return string+".";
+	}
+}
+
+function arrFormatNoAnd(arr){
+	var string = "";
+	if(arr.length == 0){
+		return "";
+	}
+	if(arr.length == 1){
+		return arr[0];
+	}
+	for(var i = 0; i < arr.length-1; i++){
+		string += arr[i] + ", ";
+	}
+	string+= arr[arr.length-1];
+
 	return string;
 }
